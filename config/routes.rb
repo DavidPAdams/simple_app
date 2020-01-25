a46d3bfc8b3a901954d1dbfc83db00e943a4a982
David Adams <david.riding.rails@gmail.com>
2020-01-24 17:43:28 -0700

    refactor user signup in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home'
+  get '/signup', to: 'users#new'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
+  
 end
