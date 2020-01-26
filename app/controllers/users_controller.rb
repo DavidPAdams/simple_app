704dcd68f0e01b19320272e771fd10eb4162fbe8
David Adams <david.riding.rails@gmail.com>
2020-01-26 15:34:42 -0700

    revise users controller with before filter for logged in user, add update method, and private logged_in_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -24,7 +25,13 @@ class UsersController < ApplicationController
   end
 
   def update
-    
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash.now[:success]="Profile successfully updated"
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   private
@@ -32,4 +39,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
 end
