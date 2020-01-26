a61b0afc594e31c4a25e855a27cc13abb64ed4ca
David Adams <david.riding.rails@gmail.com>
2020-01-26 11:29:01 -0700

    update users controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,6 +21,10 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    
   end
 
   private
