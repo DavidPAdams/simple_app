4272c68a92364cc6a4417c0ac5c5705675556ece
David Adams <david.riding.rails@gmail.com>
2020-01-25 21:31:02 -0700

    revise user class method authenticate? to return false for nil remember_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,7 @@ class User < ApplicationRecord
 
   #returen boolean of input token with the digest
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
