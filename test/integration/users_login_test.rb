ce6bb46c9c3c71bc2dea4646ea6fb40fe3493dff
David Adams <david.riding.rails@gmail.com>
2020-01-25 19:14:52 -0700

    update users login integration tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,13 +1,31 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @valid_user = users(:valid)
+    @invalid_user = users(:invalid)
+  end
+
   test "login with invalid credentials" do
     get login_path
     assert_template "sessions/new"
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { session: { email: @invalid_user.email, password: "foobar" } }
     assert_template "sessions/new"
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with valid credentials" do
+    get login_path
+    assert_template "sessions/new"
+    post login_path, params: { session: { email: @valid_user.email, password: "password" } }
+    assert_redirected_to @valid_user
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@valid_user)
   end
 end
