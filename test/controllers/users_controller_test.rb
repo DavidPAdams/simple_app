1ad4c703ed2cfb641375bbdc1c4cdfabee0b9392
David Adams <david.riding.rails@gmail.com>
2020-01-26 17:47:27 -0700

    update users controller integration tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,34 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @valid_user = users(:valid)
+    @other_user = users(:teapot)
+  end
   
   test "should get new" do
     get signup_path
     assert_response :success
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as @other_user
+    get edit_user_path(@valid_user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as @other_user
+    patch user_path(@valid_user), params: {
+                                    user: { 
+                                      name: "Jack Frost",
+                                      email: "icy@snowcave.com",
+                                      password: "",
+                                      password_confirmation: "" } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 
 end
