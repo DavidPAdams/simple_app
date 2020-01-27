0beeba5c41511cc2f210986231a873fc7a39ce76
David Adams <david.riding.rails@gmail.com>
2020-01-27 14:23:40 -0700

    add user definitions in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "Example User",
+            email: "example@railstutorial.org",
+            password: "foobarbaz",
+            password_confirmation: "foobarbaz")
+
+99.times do |x|
+  name = Faker::Name.name
+  email = "example-#{x+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+              email: email,
+              password: password,
+              password_confirmation: password)
+end
