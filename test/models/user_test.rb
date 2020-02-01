15de57fb0f874b18f1edfd900e3ac9a4cf61dfc2
David Adams <david.riding.rails@gmail.com>
2020-02-01 14:01:23 -0700

    update assertion on user unit test accessing authenticated? method

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -70,7 +70,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? method should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 
 end
