856976925369ec2ac3b56dc81e37ee7d71a0eee8
David Adams <david.riding.rails@gmail.com>
2020-02-02 17:06:47 -0700

    Revert "update config environments"
    This reverts commit 4b70a913effb2e9ea2dce654f0c5552fc68c632c.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
@@ -41,9 +41,6 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
-  # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
