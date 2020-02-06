0f7375650ac8f37059c3eaa9443b0f84f0e0814e
David Adams <david.riding.rails@gmail.com>
2020-02-06 15:42:53 -0700

    experiment in troubleshooting

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,7 +3,7 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user
-      unless user.activated?
+      if user.activated? == false
         if user.authenticated?(:activation, params[:id])
           user.activate
           log_in user
