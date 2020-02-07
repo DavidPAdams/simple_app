e9b4dcdeeacd0f6cd53f71d0a82038153c61998f
David Adams <david.riding.rails@gmail.com>
2020-02-07 11:43:00 -0700

    change account activation controller method conditional and add note

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,6 +2,7 @@ class AccountActivationsController < ApplicationController
 
   def edit
     user = User.find_by(email: params[:email])
+    # if user && !user.activated? && user.authenticated?(:activation, params[:id]) - This is the line from the tutorial, but the second condition didn't work, so I took it out
     if user && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
