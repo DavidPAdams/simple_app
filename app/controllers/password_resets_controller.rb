13dcd7aa07fc47be7d9d3fbb0cdad9daee4fa1f3
David Adams <david.riding.rails@gmail.com>
2020-02-08 19:11:35 -0700

    fix typo in password resets controller update method

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -26,7 +26,7 @@ class PasswordResetsController < ApplicationController
     if params[:user][:password].empty?
       @user.errors.add(:password, "can't be empty")
       render :edit
-    elsif @user.update_attriputes(user_params)
+    elsif @user.update_attributes(user_params)
       log_in @user
       flash[:success] = "Password has been changed"
       redirect_to @user
