ac16ad988fd695fdc15472ef4bdf3647aa69244f
David Adams <david.riding.rails@gmail.com>
2020-02-13 15:08:40 -0700

    add micropost interface integration test

---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:lana)
+  end
+
+  test "micropost interface" do
+    
+  end
+end
