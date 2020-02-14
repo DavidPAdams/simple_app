c71913b64282bedec91602940beb3e27597a5a75
David Adams <david.riding.rails@gmail.com>
2020-02-14 10:07:34 -0700

    update associations for relationships in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,14 @@ class User < ApplicationRecord
                        allow_nil: true
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   self.per_page = 5
   
