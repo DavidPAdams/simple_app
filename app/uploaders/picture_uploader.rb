bbe1709335692379ff83a5ba605ec4ffc2ee2b7e
David Adams <david.riding.rails@gmail.com>
2020-02-13 19:23:37 -0700

    revise picture uploader to include CarrierWave, resize limit, and storage location

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,10 +1,11 @@
 class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  Rails.env.production? ? storage(:fog) : storage(:file)
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
