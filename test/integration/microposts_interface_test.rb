c357bf7dba9edd4cbee8e3ee746be3d721b8d1c9
David Adams <david.riding.rails@gmail.com>
2020-02-13 17:42:30 -0700

    add assertions to microposts interface integration test

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -7,6 +7,31 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   end
 
   test "micropost interface" do
-    
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    #invalid micropost submit
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: " " } }
+    end
+    assert_select 'div#error_explanation'
+    #valid micropost submit
+    content = "This is valid content for a micropost"
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, params: { micropost: { content: content } }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body
+    #delete a micropost
+    assert_select 'a', text: 'delete'
+    first_micropost = @user.microposts.most_recent.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+    #no delete links on different user page
+    get user_path(users(:archer))
+    assert_select 'a', text: 'delete', count: 0
   end
+
 end
