e3fa6c8e9152feab595edc03325c8ba4d6044db4
David Adams <david.riding.rails@gmail.com>
2020-02-14 18:33:25 -0700

    change routes file for followers

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
