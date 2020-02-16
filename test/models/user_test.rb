f7d5b7205f261308b19c3533dab2f7e8fa85794b
David Adams <david.riding.rails@gmail.com>
2020-02-16 16:25:13 -0700

    add user unit test for micropost feed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -92,4 +92,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not lana.following?(malory)
   end
 
+  test "feed should have correct microposts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+    # microposts from a followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)      
+    end
+    # microposts from oneself
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # no microposts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
 end
